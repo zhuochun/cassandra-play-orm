604e7c8e049c05a44996f627988683bed81be8b9
Wang Zhuochun <zhuochun@hotmail.com>
2015-05-10 19:25:34 +0800

    init commit

---
+++ src/main/java/com/bicrement/cassandra/play/App.java
@@ -0,0 +1,13 @@
+package com.bicrement.cassandra.play;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
